4244a8d88ccf228f7d4349898eb730aaa2188dc0
peacelove1031 <a0955496116@gmail.com>
2024-06-08 22:17:38 +0800

    更新主頁跑馬燈及主頁圖片、影片連結問題
    更新主頁介面

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('文檔已加載');
 });
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var slides = document.getElementsByClassName("custom-header-image");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
